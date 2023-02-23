01fa168d00933aa5d575f087a91b69164d616b53
0x1d107 <yablinchik@gmail.com>
2023-02-23 23:02:01 +0500

    Replace wario with curl -Z

--- topics.sh
+++ topics.sh
@@ -3,11 +3,11 @@ SECTIONS=(1950 2200 1808 1840 1864 1810 930 209)
 mkdir -p topics
 cd topics
 echo ${SECTIONS[*]}|tr ' ' '\n'|xargs -I '{}' curl --compressed 'https://api.rutracker.cc/v1/static/pvc/f/{}' \
-|jq -r '.result|keys[]'|pr -t100 -s',' -W 9001|tr -d '\t '\
-|sed 's|^|https://api.rutracker.cc/v1/get_tor_topic_data?by=topic_id\&val=|'\
-| ../wario -c 16 --rename '{index}.json' -f '-' 
+|jq -r '.result|keys[]'|xargs -n 100 echo |tr ' ' ','\
+|awk '{print "https://api.rutracker.cc/v1/get_tor_topic_data?by=topic_id&val="$0,"-o",NR".json"}'\
+| xargs curl -ZL --parallel-max 16 --retry 5
 cd ..
-cat topics/*.json |jq -r \
-    '.result|select (. != null)|.[]|select(.!=null)|([.info_hash,.topic_title])|@csv'>topics.csv
+jq -r \
+    '.result|select (. != null)|.[]|select(.!=null)|([.info_hash,.topic_title])|@csv' topics/*.json >topics.csv
 sqlite3 -init topics.sql topics.db ''
 rm -r topics topics.csv
